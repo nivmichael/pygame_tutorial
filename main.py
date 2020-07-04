7e756a6b6301a016cfab115de7236ef14bdef3a2
AcadeMeData <michael@acade-me.com>
2020-07-04 11:23:00 +0300

    Player movement and added enemy obj

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Init
 pygame.init()
@@ -15,6 +16,13 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_Change = 0
+
+# Add Enemy
+EnemyImg = pygame.image.load('ufo.png')
+EnemyX = random.randint(0, 800)
+EnemyY = random.randint(50, 150)
+EnemyX_Change = 0
 
 
 # Player function
@@ -22,22 +30,38 @@ def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
+# Player function
+def enemy(x, y):
+    screen.blit(EnemyImg, (x, y))
+
+
 # Game Loop
 running = True
 while running:
     # RGB - build initial screen
     screen.fill((0, 0, 0))
-
+    # Game event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # Player Keystrokes
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                playerX += 5
-                player(playerX, playerY)
             if event.key == pygame.K_LEFT:
-                playerX -= 5
-                player(playerX, playerY)
+                playerX_Change -= 0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_Change += 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_Change = 0
+
+    # Change player location on map
+    playerX += playerX_Change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX > 736:
+        playerX = 736
 
     player(playerX, playerY)
+    enemy(EnemyX, EnemyY)
     pygame.display.update()
